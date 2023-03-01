337d950c9274e38714109a0ef8f5b3e759d1c997
VictorAndPolo <victorandrespolo.andy@hotmail.com>
2023-02-28 23:17:17 -0300

    117

--- models/connection.php
+++ models/connection.php
@@ -1,4 +1,4 @@
 <?php
-$conexion= new mysqli("localhost:3306","credenciales","ZpGo*seisNeuronas!x7","users");
+$conexion= new mysqli("localhost","usuarios","micasita123","lemaviic_");
 $conexion->set_charset("utf8");
 ?>
