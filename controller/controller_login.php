e9610def782d04bc18d5a458e81b9f97a937f563
VictorAndPolo <victorandrespolo.andy@hotmail.com>
2023-03-01 23:57:02 -0300

    1156

--- controller/controller_login.php
+++ controller/controller_login.php
@@ -9,13 +9,20 @@ if(!empty($_POST["btnin"])){
         $pass= $_POST["pass"];
         $user=clearString($user);
         $pass=clearString($pass);
-        $sql=$conexion->query("select * from lemaviic_.users where user='$user' and password='$pass'");
+        try {
+             $sql=$conexion->query("select * from lemaviic_.users where user='$user' and password='$pass'");
+             
         if ($data=$sql->fetch_object()) {
             $_SESSION["nombre"]=$data->user;
             header("location:index.php");
         } else {
             echo "<div class='alert alert-danger'>acceso denegado</div>";
         }
+        
+            } catch (\Exception $e) {
+           echo "Recargar la pagina o contactar con tecnico@lemavii.com.ar";
+        }
+      
 
     } else {
         echo "introducir campos";
