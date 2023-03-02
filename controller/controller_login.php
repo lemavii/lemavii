cc5c80730e381fda1c80ac562a0d8d88bd7bca49
VictorAndPolo <victorandrespolo.andy@hotmail.com>
2023-03-01 21:48:16 -0300

    948

--- controller/controller_login.php
+++ controller/controller_login.php
@@ -4,7 +4,7 @@ if(!empty($_POST["btnin"])){
     if (!empty($_POST["user"]) and !empty($_POST["pass"])) {
         $user= $_POST["user"];
         $pass= $_POST["pass"];
-        $sql=$conexion->query("select * from lmvtest.users where user='$user' and password='$pass'");
+        $sql=$conexion->query("select * from lemaviic_.users where user='$user' and password='$pass'");
         if ($data=$sql->fetch_object()) {   
             $_SESSION["nombre"]=$data->user;
             header("location:index.php");
