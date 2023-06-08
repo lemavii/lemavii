77824ec45c2f8e3d2e7096e1a6c5fd5be822f294
lemavii <lemavii.electronica@gmail.com>
2023-06-08 00:01:55 -0300

    1201

--- three.js
+++ three.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js'
+import * as THREE from './node_modules/three/build/three.module.js'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 const canvas = document.querySelector('#webgl');
 const renderer = new THREE.WebGLRenderer({canvas:canvas,antialias:true});
